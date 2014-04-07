f1a375c10e72c6a5c1c9f7c1b168bb6179bfb362
Sébastien MALOT <smalot@actualys.com>
2014-04-07 15:14:53 +0200

    add license into headers file

--- src/Smalot/Ldap/Tools.php
+++ src/Smalot/Ldap/Tools.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * @file
+ *          PHP library which handle LDAP data. Can parse too LDIF file.
+ *
+ * @author  Sébastien MALOT <sebastien@malot.fr>
+ * @license MIT
+ * @url     <https://github.com/smalot/ldap>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Smalot\Ldap;
 
 /**
